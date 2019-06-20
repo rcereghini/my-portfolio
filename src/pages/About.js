53e39bd15096729007d79297966891676b92eaf8
rcereghini <robertc@petroleumrx.com>
2019-06-19 17:43:00 -0700

    Updated about

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import pic4 from '../assets/images/pic04.jpg';
 import mma from '../assets/images/mma.jpg';
-import elliot from '../assets/images/elliot.jpg';
+import family from '../assets/images/family.jpg';
 import drawing from '../assets/images/drawing.jpg';
 import hike from '../assets/images/hike.jpg';
 import piano from '../assets/images/piano.jpg';
@@ -45,90 +45,93 @@ const IndexPage = () => (
         <div className="inner">
           <h3 className="major">Gallery</h3>
           {/* <div className="gallery"> */}
-          <section className="gallery">
+          <section className="features gallery">
             <article>
               <span className="image">
-                <img src={mma} alt="" />
+                <img src={family} alt="" />
               </span>
-              <h3 className="major">Face Finder</h3>
+              <h3 className="major">The Family</h3>
               <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
+                As far as getting everyone to look and smile at the same time,
+                this was the best we could do...
               </p>
             </article>
-            <article>
-              <span className="image">
-                <img src={elliot} alt="" />
-              </span>
-              <h3 className="major">Face Finder</h3>
-              <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
-              </p>
-            </article>
-            <article>
-              <span className="image">
-                <img src={drawing} alt="" />
-              </span>
-              <h3 className="major">Face Finder</h3>
-              <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
-              </p>
-            </article>
-            <article>
-              <span className="image">
-                <img src={hike} alt="" />
-              </span>
-              <h3 className="major">Face Finder</h3>
-              <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
-              </p>
-            </article>
-            <article>
-              <span className="image">
-                <img src={piano} alt="" />
-              </span>
-              <h3 className="major">Face Finder</h3>
-              <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
-              </p>
-            </article>
-            <article>
-              <span className="image">
-                <img src={ragnar} alt="" />
-              </span>
-              <h3 className="major">Face Finder</h3>
-              <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
-              </p>
-            </article>
+
             <article>
               <span className="image">
                 <img src={kids} alt="" />
               </span>
-              <h3 className="major">Face Finder</h3>
+              <h3 className="major">My Boys</h3>
               <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
+                I became a father three years ago, and now I've got two little
+                dudes to hang out with!
               </p>
             </article>
+
+            <article>
+              <span className="image">
+                <img src={piano} alt="" />
+              </span>
+              <h3 className="major">Playing Piano</h3>
+              <p>
+                Began self-teaching myself piano years ago. Now I'm able to pass
+                the skill on to my children.
+              </p>
+            </article>
+
+            <article>
+              <span className="image">
+                <img src={mma} alt="" />
+              </span>
+              <h3 className="major">Mixed Martial Arts</h3>
+              <p>
+                I trained consistently for over eight years. While I spend more
+                time pushing keys these days, I am still a big fan of the sport.
+              </p>
+            </article>
+
+            <article>
+              <span className="image">
+                <img src={drawing} alt="" />
+              </span>
+              <h3 className="major">Drawing</h3>
+              <p>
+                Chalkboard art for a Tempe pub during the Pokemon Go launch.
+              </p>
+            </article>
+
+            <article>
+              <span className="image">
+                <img src={hike} alt="" />
+              </span>
+              <h3 className="major">Hiking</h3>
+              <p>
+                Hiking is my favorite outdoor activity. This picture is from my
+                Grand Canyon hike!
+              </p>
+            </article>
+
+            <article>
+              <span className="image">
+                <img src={ragnar} alt="" />
+              </span>
+              <h3 className="major">Running</h3>
+              <p>Running into the finish line during a recent Ragnar run.</p>
+            </article>
+
             <article>
               <span className="image">
                 <img src={comicon} alt="" />
               </span>
-              <h3 className="major">Face Finder</h3>
+              <h3 className="major">All Things Nerdy</h3>
               <p>
-                A facial recognition app using React, Redux, Node.js, Clarifai,
-                Tachyons, Particles, Tilt, and Heroku.
+                From a year I attended Comicon. I am a big fan of sci-fi,
+                fantasy, and all things nerdy!
               </p>
             </article>
           </section>
-          {/* </div> */}
         </div>
+        {/* </div> */}
       </div>
     </section>
   </Layout>
