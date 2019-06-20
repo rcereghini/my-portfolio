262426156f01cde044367bb0cdee6620c8368a28
rcereghini <robertc@petroleumrx.com>
2019-06-19 17:06:38 -0700

    Updated manifest starter URL

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   siteTitle: 'Robert Cereghini - Web Developer', // <title>
   manifestName: 'Robert Cereghini - Web Developer',
   manifestShortName: 'rcereghini', // max 12 characters
-  manifestStartUrl: 'https://www.rcereghini.com/',
+  manifestStartUrl: '/',
   manifestBackgroundColor: '#1c1d27',
   manifestThemeColor: '#1c1d27',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/rcIcon.jpg',
-  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  // pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Robert Cereghini',
   subHeading:
     'Full-Stack Web Developer who is able to wear many hats. Strong worker and fast, self-motivated learner with exceptional written communication skills. Passionate about expanding my knowledge and furthering my programming skills. Excited to be in a career that is constantly evolving.',
